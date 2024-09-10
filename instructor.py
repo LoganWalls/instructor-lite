62be487be5e82b4920336765fc8352dda80d23d1
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-09-10 11:58:33 -0400

    add license directly into source code

--- instructor.py
+++ instructor.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2024 Logan A. Walls
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from collections import OrderedDict
 from typing import List, Literal, Optional, Sequence, Tuple
 
@@ -63,7 +87,9 @@ class InstructorModel:
         # Instructor models contain an additional layer that isn't part of the T5 architecture.
         # this loads that additional layer from the checkpoint.
         self.output_layer = torch.nn.Linear(
-            self.hf_model.config.d_model, 768, bias=False  # type: ignore
+            self.hf_model.config.d_model,
+            768,
+            bias=False,  # type: ignore
         )
         output_weights_path: str = hub.cached_file(  # type: ignore
             model_name, "2_Dense/pytorch_model.bin"
